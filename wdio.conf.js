c1bb58a99e6eb9e01132ce9d41f998105869c6b8
Amit Rana <amit.rana@agilitypr.com>
2022-07-25 05:53:51 +0530

    commented firefox code

--- wdio.conf.js
+++ wdio.conf.js
@@ -81,11 +81,11 @@ exports.config = {
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-    },
-    {
-        maxInstances: 2,
-        browserName: 'firefox'
-    }
+    }//,
+    // {
+    //     maxInstances: 2,
+    //     browserName: 'firefox'
+    // }
     ],
     //
     // ===================
